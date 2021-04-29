c29a2143542bcb7d98e84196379b049854bbdd2e
Andrepaolo <andrepaol32@gmail.com>
2021-04-29 16:49:10 -0500

    Trabajo29/04

--- Trabajo/claseArtefactos.py
+++ Trabajo/claseArtefactos.py
@@ -1,11 +1,14 @@
-class Artefactos:
-	def__init__(self, nombre, funca, mododeuso, modelo):
+class Artefacto:
+	def	__init__(self, nombre, funca, mododeuso, modelo):
 		self.nombre= nombre
 		self.funca=funca
 		self.mododeuso=mododeuso
 		self.modelo=modelo
-	def atributo()
+	def decir():
 		return f'El/la {self.nombre}, funciona con {self.funca}, es de uso {self.funca} y su modelo es {self.modelo}'
-Horno = Artefactos('Horno','gas','manual','artesanal')
-Maquinadecoser = Artefactos('Maquinadecoser','electricidad','manual','Precious 590')
-Freidoradeaire= Artefactos('Freidoradeaire','electricidad','automatico','Oster2020')
+Horno= Artefacto('Horno','gas','manual','artesanal')
+Maquinadecoser = Artefacto('Maquinadecoser','electricidad','manual','Precious 590')
+Freidoradeaire= Artefacto('Freidoradeaire','electricidad','automatico','Oster2020')
+print(Horno.decir)
+
+
